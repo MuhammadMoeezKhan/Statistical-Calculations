216217a541243716d58f4c44df327692d368138d
Muhammad Moeez Khan <85444389+MuhammadMoeezKhan@users.noreply.github.com>
2022-09-05 12:08:18 -0400

    Add files via upload

---
+++ Source-Code/StatsModelling.py
@@ -0,0 +1,140 @@
+import pandas as pd
+
+"""
+@author: moeezkhan
+"""
+
+"""
+delimiter: seperator of content whule parsing a file = sep --> java regex
+header: int list --> row numbers
+name: array --> coloumn names
+nrows : number of files to read
+"""
+
+#Getting ages from the adult.data file
+def getAgeList(numRows = None):
+    
+    colNames = ["age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation", "relationship", "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country", "income"]
+    
+    origData = pd.read_csv("data/adult.data", delimiter = ", ", header=None, names=colNames, engine='python', nrows=5)
+    
+    return origData.loc[:, 'age'].tolist()
+
+
+#Get all ages from adult.data
+def main():
+    
+    #getting the list of ages from the provided function
+    ages = getAgeList()
+        
+    #updating the ages list by each element's zScore to perform standardization
+    stdAges = standardize(ages)
+    
+    #storing the mean returned by the calcMean function after passing the age list as an argument
+    mean = calcMean(ages)
+    
+    #storing the standard deviation returned by the calcStdDev after passing the list of ages and the mean of the ages
+    stdDev = calcStdDev(ages, mean)
+    
+    #storing the mode returned by the calcMode function after passing in the list of ages
+    mode = calcMode(ages)
+    
+    
+    #printing the answer 
+    """
+    print("Original List: ", ages)
+    print("Standardized List: ", stdAges)
+    print("Mean: ", mean)
+    print("StdDev: ", stdDev)
+    print("Move: ", mode)
+    """
+
+#Calculating the mean of all ages in the adult.data file
+def calcMean(ages):
+    
+    #getting the length of the list = total number of ages 
+    totalAges = len(ages)
+    
+    #variable used to sum up  all ages
+    sum = 0
+    
+    #traversing the entire list
+    for age in ages:
+        sum += age
+    
+    #calculating the mean using the sum
+    mean = sum / totalAges
+    
+    return mean
+ 
+    
+#Calculating the Standard Deviation
+def calcStdDev(numbers, mean):
+    
+    length = len(numbers)
+    sumMeanDifference = 0
+    
+    for i in range(0,length, 1):
+        meanDifference = numbers[i] - mean
+        sqMeanDifference = meanDifference ** 2
+        sumMeanDifference += sqMeanDifference
+        
+    stdDevSq = (1 / (length - 1)) * sumMeanDifference
+    
+    return stdDevSq ** (1/2)
+    
+
+#Calculating the Mode
+def calcMode(numbers):
+    numFreq = {}
+    
+    #Track frequencies
+    for number in numbers:
+        if number in numFreq:
+            numFreq[number] += 1
+            
+        else:
+            numFreq[number] = 1
+    
+    #Variables to track data 
+    maxFreq = -1
+    mode = -1
+    
+    #Traverse the Dictionary
+    #Find key with the max frequecy(value)
+    for k, v in numFreq.items():
+        if v > maxFreq:
+            mode = k
+            maxFreq = v
+        
+    return mode
+            
+
+# Performing Standardization
+def standardize(numbers):
+    
+    mean = calcMean(numbers)
+    stdDev = calcStdDev(numbers, mean)
+    length = len(numbers)
+        
+    for i in range(0, length, 1):
+        meanDiff = numbers[i] - mean;
+        zScore = meanDiff / stdDev
+        numbers[i] = zScore
+    
+    return numbers
+     
+    
+#calling the main function for testing purposes
+main()
+
+
+"""
+Python 2x and 3x Differences:
+    - print statement
+    - 1 / 2 gives a float  and  1 // 2 gives an int
+    - != not equals (as in Java)
+    - input to read Strings
+    - All classes must explicity extend the Ojbect class
+"""
+
